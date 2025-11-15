6facfab639f02793a8d881d123986fe29d2f92fb
Bryan <buungoc1805@gmail.com>
2025-11-15 13:44:36 +0700

    Learned Numpy Data Types

--- Python/numpy_learning/numpy_learning.py
+++ Python/numpy_learning/numpy_learning.py
@@ -1,5 +1,7 @@
 import numpy as np
 
-arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+arr = np.array([1.1, 2.1, 3.1])
+new_arr = arr.astype('i')
 
-print(arr[0:2, 1:4])
+print(new_arr)
+print(new_arr.dtype)
