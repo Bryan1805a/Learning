ab628faa15d7a9504dd87fbccba5b9a1a537b86f
Bryan <buungoc1805@gmail.com>
2025-11-15 08:49:58 +0700

    Learned Numpy Array Indexing

--- Python/numpy_learning/numpy_learning.py
+++ Python/numpy_learning/numpy_learning.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 5], ndmin=5)
+arr = np.array([[1, 2, 3, 4, 5,], [6, 7, 8, 9, 10]])
 
-print(arr)
-print(type(arr))
-print(arr.ndim)
+print(arr[1, -1])
