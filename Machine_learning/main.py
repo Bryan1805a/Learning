2e351081295c13e86db53a92aaaf7c410b8a4319
Bryan <buungoc1805@gmail.com>
2025-12-07 22:41:42 +0700

    .

--- Machine_learning/main.py
+++ Machine_learning/main.py
@@ -1,39 +1,51 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 import pandas as pd
+import numpy as np
 
-data = pd.read_csv(r'Machine_learning\Data\study.csv')
+
+dataset = pd.read_csv(r'Machine_learning\Data\social_network_ads.csv')
+
+X = dataset.iloc[:,[2, 3]].values
+y = dataset.iloc[:,-1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
 
-def gradient_descent(m_now, b_now, points, L):
-    m_gradient = 0
-    b_gradient = 0
-    
-    n = len(points)
-    
-    for i in range(n):
-        x = points.iloc[i].study_time
-        y = points.iloc[i].score
-        
-        m_gradient += -(2/n) * x * (y - (m_now * x + b_now))
-        b_gradient += -(2/n) *  (y - (m_now * x + b_now))
-    
-    m = m_now - m_gradient * L;
-    b = m_now - b_gradient * L;
-    
-    return m, b
 
-m = 0
-b = 0
-L = 0.0001
-epochs = 300
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
-for i in range(epochs):
-    if i % 50 == 0:
-        print(f"Epoch: {i}")
-    m, b = gradient_descent(m, b, data, L)
-    
-print(m, b)
 
-plt.scatter(data.study_time, data.score, color='black', )
-plt.plot(list(range(20, 80)), [m * + b for x in range(20, 80)], color='red')
+model = LogisticRegression(random_state=0)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+acc = accuracy_score(y_test, y_pred)
+
+X_set, y_set = sc.inverse_transform(X_test), y_test
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min()-10, stop=X_set[:, 0].max()+10, step=0.25),
+                     np.arange(start=X_set[:, 1].min()-1000, stop=X_set[:, 1].max()+1000, step=0.25))
+
+plt.contourf(X1, X2, model.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('red', 'green')))
+
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set==j, 0], X_set[y_set==j, 1],
+                c=ListedColormap(('red', 'green'))(i), label=j)
+
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
 plt.show()
