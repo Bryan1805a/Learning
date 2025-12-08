a56e066906be2e25d212964a6de2d76b60d2b4e1
Bryan <buungoc1805@gmail.com>
2025-12-08 17:08:19 +0700

    .

--- Machine_learning/main.py
+++ Machine_learning/main.py
@@ -1,51 +1,50 @@
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix, accuracy_score
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-import pandas as pd
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+# Read file
+df = pd.read_csv(r'Machine_learning\Data\student_info.csv')
+# print(df)
+# print(df.head())
+# print(df.tail())
+# print(df.shape)
+# print(df.info())
+# print(df.describe())
 
-dataset = pd.read_csv(r'Machine_learning\Data\social_network_ads.csv')
+'''
+plt.figure(figsize=(7, 5))
+plt.scatter(x=df.study_hours, y=df.student_marks, color='red', marker='*', s=100, alpha=0.5)
+plt.xlabel('Study_hours')
+plt.ylabel("Student_marks")
+plt.title('Test data')
+plt.show()
+'''
 
-X = dataset.iloc[:,[2, 3]].values
-y = dataset.iloc[:,-1].values
+# print(df.isnull())
+# print(df.isnull().sum())
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+# print(df.mean())
 
+df_2 = df.fillna(df.mean())
+# print(df_2.isnull().sum())
 
+# Heatmap
+'''
+plt.figure(figsize=(8, 5))
+sns.heatmap(df.corr(), annot=True, linewidths=3)
+plt.show()
+'''
+'''
+plt.figure(figsize=(8, 5))
+sns.barplot(x='study_hours', y='student_marks', data=df_2)
+plt.show()
+'''
 
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+X = df_2.drop('student_marks', axis='columns')
+y = df_2.drop('study_hours', axis='columns')
+# print(X.shape)
+# print(y.shape)
 
-
-model = LogisticRegression(random_state=0)
-model.fit(X_train, y_train)
-
-y_pred = model.predict(X_test)
-
-cm = confusion_matrix(y_test, y_pred)
-acc = accuracy_score(y_test, y_pred)
-
-X_set, y_set = sc.inverse_transform(X_test), y_test
-X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min()-10, stop=X_set[:, 0].max()+10, step=0.25),
-                     np.arange(start=X_set[:, 1].min()-1000, stop=X_set[:, 1].max()+1000, step=0.25))
-
-plt.contourf(X1, X2, model.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
-             alpha=0.75, cmap=ListedColormap(('red', 'green')))
-
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set==j, 0], X_set[y_set==j, 1],
-                c=ListedColormap(('red', 'green'))(i), label=j)
-
-plt.title('Logistic Regression (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
+sns.pairplot(df)
 plt.show()
