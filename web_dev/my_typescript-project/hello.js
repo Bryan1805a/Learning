88971bbd0462eec2d4687ffd32beff6a693023af
Bryan <buungoc1805@gmail.com>
2026-03-30 21:55:11 +0700

    .

---
+++ web_dev/my_typescript-project/hello.js
@@ -0,0 +1,6 @@
+"use strict";
+function calculator(a, b) {
+    return a + b;
+}
+let a = calculator(1, 2);
+console.log(a);
---
+++ web_dev/my_typescript-project/hello.ts
@@ -0,0 +1,3 @@
+export default function calculate(a: number, b: number) {
+    return a + b;
+}
